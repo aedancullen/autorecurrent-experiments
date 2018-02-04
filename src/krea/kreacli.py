5aa90a2e000970bdcaf89882a1217ecdc0db9b12
Aedan Cullen <aedancullen@gmail.com>
2018-02-03 22:43:20 -0500

    Add argparsing

--- src/krea/kreacli.py
+++ src/krea/kreacli.py
@@ -1,5 +1,6 @@
 from krea import krealearn
-
+import argparse
+import os
 
 def start_cli():
 
@@ -7,11 +8,38 @@ def start_cli():
 	cli.run()
 
 
+def isfile(parser, fn):
+    if not os.path.isfile(fn):
+        parser.error('{} is not a file'.format(fn))
+    else:
+        return fn
+
+
+def isdir(parser, fn):
+    if not os.path.isdir(fn):
+        parser.error('{} is not a directory'.format(fn))
+    else:
+        return fn
+
+
 class KreaCLI:
 
+	parser = None
+
 	def __init__(self):
-		pass
+		self.parser = argparse.ArgumentParser(
+			prog = "krea",
+			description="Krea Recurrent-Evolving AGI\ngithub.com/aedancullen/krea"
+		)
+		self.parser.add_argument("problemsdir", type=lambda x:isfile(parser,x))
+		self.parser.add_argument("programfile", type=lambda x:isdir(parser,x))
 
 
-	def run_krealearn(self, practice_fn, program_fn):
-		pass
+	def run(self) :
+		args = self.parser.parse_args()
+		problems_fn = args.problemsdir
+		program_fn = args.programfile
+		self.run_krealearn(self, problems_fn, program_fn)
+
+	def run_krealearn(self, problems_fn, program_fn):
+		learner = krealearn.KreaLearn()
